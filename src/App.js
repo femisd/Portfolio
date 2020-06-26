86ca364e12aaebcfcbd3e748f2ade3c828bf8824
femisd <femisd@gmail.com>
2020-06-26 18:44:26 +0100

    scroller

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import Intro from './components/Intro/Intro'
 import AboutMe from './components/AboutMe/AboutMe';
 import Projects from './components/Projects/Projects';
+import * as Scroll from 'react-scroll';
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 function App() {
   return (
     <div className="App">
-       <div className="navDiv">
-                <ul className="navBar">
-                    <li className="navElement">
-                        <a>Hackathons</a>
-                    </li>
-                    <li className="navElement">
-                        <a>Projects</a>
-                    </li>
-                    <li className="navElement">
-                        <a>About me</a>
-                    </li>
-                </ul>
+      <div className="navDiv">
+        <ul className="navBar">
+          <li className="navElement">
+            <a
+            // onClick={() => scroller.scrollTo('Intro', {
+            //   duration: 1500,
+            //   delay: 100,
+            //   smooth: true,
+            //   offset: 0,
+            // })}
+            >Hackathons</a>
+          </li>
+          <li className="navElement"
+            onClick={() => scroller.scrollTo('Projects', {
+              duration: 1500,
+              delay: 100,
+              smooth: true,
+              offset: -50,
+            })}>
+            <a>Projects</a>
+          </li>
+          <li className="navElement"
+            onClick={() => scroller.scrollTo('AboutMe', {
+              duration: 1500,
+              delay: 100,
+              smooth: true,
+              offset: -50,
+            })}>
+            <a>About me</a>
+          </li>
+        </ul>
       </div>
-      <Intro />
-      <AboutMe />
-      <Projects />
+      <Element name="Intro">
+        <Intro />
+      </Element>
+
+      <Element name="AboutMe">
+        <AboutMe />
+      </Element>
+
+      <Element name="Projects">
+        <Projects />
+      </Element>
     </div>
   );
 }
